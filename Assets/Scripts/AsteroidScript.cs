cb5640ce6637e79701c074448dc2057c4805f8dc
Alexandre Lazare Lyon-Szwajka <LazAl070@school.lu>
2022-03-05 21:20:15 +0100

    Asteroid prefab + scripts

--- Assets/Scripts/AsteroidScript.cs
+++ Assets/Scripts/AsteroidScript.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class AsteroidScript : MonoBehaviour
 {
+    private Vector2 screenBoundsTopRight, screenBoundsTopLeft, screenBoundsBottomLeft, screenBoundsBottomRight;
     public GameObject player;
+    public Rigidbody2D RB;
+    private float moveSpeed,rotSpeed, rotZ;
+    private Vector2 dir;
+
+    public SpriteRenderer rend;
+    public Sprite[] sprites;
     // Start is called before the first frame update
     void Start()
     {
+        rend.sprite = sprites[Random.Range(0, sprites.Length - 1)];
+        moveSpeed = 0.005f;
+        rotSpeed = 20f;
         Vector2 dir = player.transform.position - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(dir * moveSpeed);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<Rigidbody2D>().AddForce(transform.forward * moveSpeed);
+        rotZ += Time.deltaTime * rotSpeed;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+        if (Vector2.Distance(transform.position, player.transform.position) > 10f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            //add damage function plz, gracias
+            //player.
+        }    
     }
 }
